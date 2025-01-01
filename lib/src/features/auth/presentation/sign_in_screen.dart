b021c2e63f23dcd05c487dc02bd10139c87ad741
Derodian <derodian@gmail.com>
2025-01-01 14:37:21 +0530

    auth starter

---
+++ lib/src/features/auth/presentation/sign_in_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SignInScreen extends ConsumerWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sign In')),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text('Sign In'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
